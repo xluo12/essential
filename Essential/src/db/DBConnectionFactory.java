db4dcf94a7631c41d81a7e79030a75bb2a66cf17
XiaoyuLuo <xluo69339@gmail.com>
2018-04-20 00:22:20 -0400

    add MySQL table creation

--- Essential/src/db/DBConnectionFactory.java
+++ Essential/src/db/DBConnectionFactory.java
@@ -1,5 +1,25 @@
 package db;
 
+import db.mysql.MySQLConnection;
+
 public class DBConnectionFactory {
+	// This should change based on the pipeline.
+	private static final String DEFAULT_DB = "mysql";
+	// This should change based on the pipeline.
+
+	public static DBConnection getConnection(String db) throws IllegalArgumentException {
+		switch (db) {
+		case "mysql":
+			return new MySQLConnection(); // new MySQLConnection()
+		case "mongodb":
+			return null; // new MongoDBConnection()
+		default:
+			throw new IllegalArgumentException("Invalid db: " + db);
+		}
+	}
+
+	public static DBConnection getConnection() throws IllegalArgumentException {
+		return getConnection(DEFAULT_DB);
+	}
 
 }
--- Essential/src/rpc/SearchItem.java
+++ Essential/src/rpc/SearchItem.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -14,6 +15,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
 import entity.Item;
 import external.TicketMasterAPI;
 
@@ -41,18 +44,22 @@ public class SearchItem extends HttpServlet {
 		
 		// term can be empty
 		String keyword = request.getParameter("term");
-		TicketMasterAPI tmAPI = new TicketMasterAPI();
-		List<Item> items = tmAPI.search(lat, lon, keyword);
 		
-		JSONArray array = new JSONArray();
+		DBConnection connection = DBConnectionFactory.getConnection();
+		List<Item> items = connection.searchItems(lat, lon, keyword);
+        connection.close(); 
+
+        List<JSONObject> list = new ArrayList<>();
 		try {
 			for (Item item : items) {
+				// Add a thin version of item object
 				JSONObject obj = item.toJSONObject();
-				array.put(obj);
+				list.add(obj);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		JSONArray array = new JSONArray(list);
 		RpcHelper.writeJsonArray(response, array);
 	}
 
